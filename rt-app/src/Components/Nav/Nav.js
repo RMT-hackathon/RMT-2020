52ee8c7487d00b75c427913ca9b6f26e4e492a9c
Clark Williams <theclarkwilliams@gmail.com>
2020-01-02 13:19:12 -0700

    added scss file structure

--- rt-app/src/Components/Nav/Nav.js
+++ rt-app/src/Components/Nav/Nav.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    
+    const loggedIn = true;
+    const logout = () => {
+
+    }
+    const signup = () => {
+
+    }
+    const login = () => {
+
+    }
     const links = [
         {
             name: "About",
             path: "/about"
+        },
+        {
+            name: "FAQ",
+            path: "/faq "
         }
     ]
     
     return (
         <nav>
-            
+            <div className="logo">
+                <img />
+            </div>
+            {links.map(l => {
+            return <Link key={l.name} to={l.path}>{l.name}</Link>
+            })}
+            {
+                loggedIn ? (
+                    <div className="btn" onClick={logout}>Logout</div> 
+                )
+                : (
+                    <>
+                        <div className="btn" onClick={signup}>Sign Up</div>
+                        <div className="btn" onClick={login}>Log In</div>
+                    </>
+                )
+            }
         </nav>
     );
 };
