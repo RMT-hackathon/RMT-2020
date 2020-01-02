7ad99d031a55b81f765afeeed5219fd385a68e86
SpencerElggren <elggren.spencer@gmail.com>
2020-01-02 12:38:45 -0700

    footer images added

--- rt-app/src/Components/Footer/Footer.js
+++ rt-app/src/Components/Footer/Footer.js
@@ -3,10 +3,14 @@ import React from 'react';
 const Foot = () => {
     return (
         <div>
-            <img/>
-
+            <img src={'rt-app/src/img/footer_logo.svg'}/>
+            <div>
+                <h2>Contact</h2>
+                <h3>info@company.com</h3>
+                <p>+1(999)999-999</p>
+            </div>
         </div>
     )
-}
+};
 
 export default Foot;
