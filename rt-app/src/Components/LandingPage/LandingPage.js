1f26b5f2c8e4ca125daf4c43fab968c00ed912b8
SpencerElggren <elggren.spencer@gmail.com>
2020-01-02 22:23:06 -0700

    Sections added, SecondSection.js

--- rt-app/src/Components/LandingPage/LandingPage.js
+++ rt-app/src/Components/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TitleSection from "./title_section/TitleSection";
+import Second from "./second_section/SecondSection";
 
 const LandingPage = () => {
     return (
         <div>
             <TitleSection/>
+            <Second/>
         </div>
     );
 };
--- rt-app/src/Components/LandingPage/second_section/SecondSection.js
+++ rt-app/src/Components/LandingPage/second_section/SecondSection.js
@@ -1,4 +1,8 @@
 import React from "react";
+import layouts from '../../../img/icons/layouts.svg'
+import awards from '../../../img/icons/awards.svg'
+import coffee from '../../../img/icons/coffee.svg'
+import projects from '../../../img/icons/projects.svg'
 
 const Second = () => {
     return (
@@ -6,13 +10,29 @@ const Second = () => {
             <div className={'textbox'}>
                 <h1>New Best Experience</h1>
                 <p>Nam natum volutpat eu. Natum elitr vel te. Id qui purto dicit, bonorum minimum et sit. Cum ei assum tation homero, at per assum dicit verterem. Lorem deterruisset.</p>
-                <btn>Explore</btn>
+                <button>Explore</button>
             </div>
             <div className={'boxItems'}>
-                <div className={'box'} id={'layouts'}></div>
-                <div className={'box'} id={'awards'}></div>
-                <div className={'box'} id={'coffee'}></div>
-                <div className={'box'} id={'projects'}></div>
+                <div className={'box'} id={'layouts'}>
+                    <img src={layouts}/>
+                    <h2>Layouts</h2>
+                    <p>Quidam officiis similique sea ei, vel tollit indoctum efficiendi tantas platonem. </p>
+                </div>
+                <div className={'box'} id={'awards'}>
+                    <img src={awards}/>
+                    <h2>Awards</h2>
+                    <p>Deseruisse definitionem his et, an has veri integre abhorreant, nam alii epicurei.</p>
+                </div>
+                <div className={'box'} id={'coffee'}>
+                    <img src={coffee}/>
+                    <h2>Cups of Coffee</h2>
+                    <p>Ea eos essent ornatus percipit, mea an persecuti pertinacia partiendo eu ius.</p>
+                </div>
+                <div className={'box'} id={'projects'}>
+                    <img src={projects}/>
+                    <h2>Projects</h2>
+                    <p>Mea iisque mentitum signiferumque te, sed commune erroribus scripserit sea.</p>
+                </div>
             </div>
         </div>
     )
